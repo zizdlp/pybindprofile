b2b5336e54da0796ef520d9aacc53b91eb96a028
zz <zdlp@sina.cn>
2023-07-31 19:09:59 +0800

    update

--- src_cpp/WrapperTest.cc
+++ src_cpp/WrapperTest.cc
@@ -10,7 +10,7 @@ uint64_t GetTimeStamp() {  // 直接调用此函数就可以返回时间戳了
       .count();
 }
 
-void TestWrapper(){
+void TestLargeWrapper(){
   std::cout << "_________  TestWrapper start ___________ \n";
   DataOwner<int> dataOwner;
   std::string arr_left="arr_left";
@@ -27,7 +27,7 @@ void TestWrapper(){
   left->set_value(3);
   right->set_value(7);
   res->set_value(5);
-  int loop=100;
+  int loop=10;
   auto start_time=GetTimeStamp();
   for(auto index=0;index<loop;++index){
       mul(left,right,res);
@@ -35,9 +35,39 @@ void TestWrapper(){
 
   auto end_time=GetTimeStamp();
   // res->print_value();
-  std::cout<<"Wrapper Test with loop"<<loop<<";length:"<<length<<"mul time consume:"<<end_time-start_time<<"ms"<<std::endl;
+  std::cout<<"Wrapper Large 1亿规模向量 Test with loop"<<loop<<";length:"<<length<<"mul time consume:"<<end_time-start_time<<"ms"<<std::endl;
+  std::cout << "_________  TestWrapper passed ___________ \n";
+}
+
+void TestSmallWrapper(){
+  std::cout << "_________  TestWrapper start ___________ \n";
+  DataOwner<int> dataOwner;
+  std::string arr_left="arr_left";
+  std::string arr_right="arr_right";
+  std::string arr_res="arr_res";
+  int length=10000;
+  dataOwner.create_array(length,arr_left);
+  dataOwner.create_array(length,arr_right);
+  dataOwner.create_array(length,arr_res);
+
+  auto left =dataOwner.get_array(arr_left);
+  auto right =dataOwner.get_array(arr_right);
+  auto res =dataOwner.get_array(arr_res);
+  left->set_value(3);
+  right->set_value(7);
+  res->set_value(5);
+  int loop=100000;
+  auto start_time=GetTimeStamp();
+  for(auto index=0;index<loop;++index){
+      mul(left,right,res);
+  }
+
+  auto end_time=GetTimeStamp();
+  // res->print_value();
+  std::cout<<"Wrapper 1w规模向量 Test with loop"<<loop<<";length:"<<length<<"mul time consume:"<<end_time-start_time<<"ms"<<std::endl;
   std::cout << "_________  TestWrapper passed ___________ \n";
 }
 int main() {
-  TestWrapper();
+  TestLargeWrapper();
+  TestSmallWrapper();
 }
--- src_pybind/run.py
+++ src_pybind/run.py
@@ -16,7 +16,7 @@ def test():
     UCharP(100000)
     UCharP(100000)
     UCharP(100000)
-def testDataWrapper():
+def testLargeDataWrapper():
     dataWrapper=DataWrapper()
     length=100000000
     dataWrapper.createData(length,"left")
@@ -24,14 +24,32 @@ def testDataWrapper():
     dataWrapper.createData(length,"res")
     dataWrapper.setValue(3,"left")
     dataWrapper.setValue(7,"right")
-    loop=100
+    loop=10
     start_time=time.time()
     for i in range(loop):
         dataWrapper.mul_operation("left","right","res")
     end_time=time.time()
     # dataWrapper.print_value("res")
-    print(f"python level mul:{length} loop:{loop} time consume:{end_time-start_time}")
+    print(f"python large data:1亿规模向量 level mul:{length} loop:{loop} time consume:{end_time-start_time}")
+
+
+def testSmallDataWrapper():
+    dataWrapper=DataWrapper()
+    length=10000
+    dataWrapper.createData(length,"left")
+    dataWrapper.createData(length,"right")
+    dataWrapper.createData(length,"res")
+    dataWrapper.setValue(3,"left")
+    dataWrapper.setValue(7,"right")
+    loop=100000
+    start_time=time.time()
+    for i in range(loop):
+        dataWrapper.mul_operation("left","right","res")
+    end_time=time.time()
+    # dataWrapper.print_value("res")
+    print(f"python small data:10w规模向量 level mul:{length} loop:{loop} time consume:{end_time-start_time}")
 
 if __name__=="__main__":
-    testDataWrapper()
+    testLargeDataWrapper()
+    testSmallDataWrapper()
     print("run is done")
